9fb2fcc25f83ce8dbcfe17ecd6b2d1110ff36aa1
MariaKundrat <03032005km@gmail.com>
2023-11-10 15:59:19 +0200

    Add lab4

--- main.py
+++ main.py
@@ -1,35 +1,74 @@
-class BinaryTree:
-    def __init__(self, value, left=None, right=None, parent=None):
-        self.value = value
-        self.left = left
-        self.right = right
-        self.parent = parent
+from queue import Queue
+
+row = [2, 2, -2, -2, 1, 1, -1, -1]
+col = [-1, 1, 1, -1, 2, -2, 2, -2]
 
 
-    def __str__(self):
-        return f'BinaryTree: {self.value}, {self.left}, {self.right}, {self.parent}'
+def generate_possible_moves(current_pos, size):
+    possible_moves = []
+    for i in range(8):
+        new_pos = (current_pos[0] + row[i], current_pos[1] + col[i])
+        if (0 <= new_pos[0] < size) and (0 <= new_pos[1] < size):
+            possible_moves.append(new_pos)
+    return possible_moves
 
 
-def find_func(tree: BinaryTree, node: BinaryTree) -> BinaryTree:
-    if node and node.right:
-        return get_right_child(node.right, node.value)
-    else:
-        return get_left_child(node, node.value)
+def build_graph(size: int, start: tuple, end: tuple):
+    global graph
+    graph = {start: []}
+    queue = Queue()
+    queue.put(start)
+    visited = set()
+
+    while not queue.empty():
+        current_pos = queue.get()
+
+        for new_pos in generate_possible_moves(current_pos, size):
+            if new_pos not in visited:
+                graph[current_pos].append(new_pos)
+                if new_pos not in graph:
+                    graph[new_pos] = []
+                queue.put(new_pos)
+                visited.add(new_pos)
+                if new_pos == end:
+                    return graph
 
 
-def get_right_child(node: BinaryTree, target_value) -> BinaryTree:
-    current_node = node
-    while current_node:
-        if current_node.value > target_value:
-            return current_node
-        current_node = current_node.right
-    return None
+def min_distance(graph, start, end) -> int:
+    if start not in graph.keys() or end not in graph.keys():
+        return -1
+
+    visited = set()
+    queue = Queue()
+    queue.put((start, 0, [start]))
+    moves = 0
+
+    while not queue.empty():
+        current_pos, depth, path = queue.get()
+        visited.add(current_pos)
+
+        for neighbor in graph[current_pos]:
+            if neighbor not in visited:
+                if neighbor == end:
+                    path.append(neighbor)
+                    print("General:", moves)
+                    print("Coordinates:", path)
+                    return depth + 1
+                queue.put((neighbor, depth + 1, path + [neighbor]))
+                moves += 1
+
+    return 0
 
 
-def get_left_child(node: BinaryTree, target_value) -> BinaryTree:
-    current_node = node
-    while current_node.parent:
-        if current_node.parent.value > target_value:
-            return current_node.parent
-        current_node = current_node.parent
-    return None
+if __name__ == '__main__':
+    with open("input.txt", "r") as file:
+        size_of_desk = int(file.readline())
+        start_point = eval(file.readline())
+        end_point = eval(file.readline())
+
+    graph = build_graph(size_of_desk, start_point, end_point)
+    min_dist = min_distance(graph, start_point, end_point)
+    print("Minimum:", min_dist)
+
+    with open("output.txt", "w") as f:
+        f.write(str(min_dist))
